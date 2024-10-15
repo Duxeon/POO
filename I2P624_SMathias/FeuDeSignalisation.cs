fbe4952d31590ba87d2de435f363f9120f38aa31
Duxeon <duxeon000@gmail.com>
2024-10-15 11:08:58 +0200

    ftech

---
+++ I2P624_SMathias/FeuDeSignalisation.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I2P624_SMathias
+{
+    internal class FeuDeSignalisation
+    {
+        int _couleur;
+        bool _etat;
+        string _identifiant;
+
+        public bool Etat
+        {
+            get { return _etat; }
+            set { _etat = value; }
+        }
+        public string Identifiant
+        {
+            get { return _identifiant; }
+        }
+        public int Couleur
+        {
+            get { return _couleur; }
+            set { _couleur = value; }
+        }
+        public FeuDeSignalisation(string name)
+        {
+            _identifiant = name;
+            _etat = false;
+            _couleur = 0;
+        }
+
+        public string AfficheTout()
+        {
+            string couleur;
+            string etat;
+            switch (_couleur)
+            {
+                case 0:
+                    couleur = "Rouge";
+                    break;
+                case 1:
+                    couleur = "Orange";
+                    break;
+                case 2:
+                    couleur = "Vert";
+                    break;
+                default:
+                    couleur = "Noir";
+                    break;
+            }
+            if (_etat) 
+            {
+                etat = "allumé";
+            }
+            else
+            {
+                etat = "éteint";
+            }
+            return "Le feu de signalisation " + _identifiant + " est " + couleur + " et " + etat + ".";
+        }
+        public string AfficheCouleur()
+        {
+            string couleur;
+            switch (_couleur)
+            {
+                case 0:
+                    couleur = "Rouge";
+                    break;
+                case 1:
+                    couleur = "Orange";
+                    break;
+                case 2:
+                    couleur = "Vert";
+                    break;
+                default:
+                    couleur = "Noir";
+                    break;
+            }
+            return "Le feu de signalisation " + _identifiant + " est " + couleur + ".";
+        }
+        public string AfficheEtat()
+        {
+            string etat;
+            if (_etat)
+            {
+                etat = "allumé";
+            }
+            else
+            {
+                etat = "éteint";
+            }
+            return "Le feu de signalisation " + _identifiant + " et " + etat + ".";
+        }
+        public void ChangeEtat()
+        {
+            _etat = !_etat;
+        }
+        public void ChangeCouleur()
+        {
+            if (_couleur == 0)
+            {
+                _couleur = 2;
+            }
+            else
+            {
+                _couleur--;
+            }
+        }
+
+        public void Clignote(int nbAlt)
+        {
+            for (int i = 0; i < nbAlt; i++)
+            {
+                ChangeEtat();
+                Console.WriteLine(AfficheEtat());
+            }
+        }
+        public void ClignoteCouleur(int nbAlt)
+        {
+            for (int i = 0; i < nbAlt; i++)
+            {
+                ChangeCouleur();
+                Console.WriteLine(AfficheCouleur());
+            }
+        }
+    }
+}
---
+++ I2P624_SMathias/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace I2P624_SMathias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FeuDeSignalisation[] fire = new FeuDeSignalisation[2];
+            fire[0] = new FeuDeSignalisation("001");
+            fire[1] = new FeuDeSignalisation("002");
+            while (true)
+            {
+                Console.WriteLine("Etat et couleur des feux : ");
+                Console.WriteLine("------------------------------------------------------------");
+                for (int i = 0; i < fire.Length; i++)
+                {
+                    Console.WriteLine(fire[i].AfficheTout());
+                }
+                Console.WriteLine("");
+                Console.WriteLine("Faire Passer le 002 à l'Orange : ");
+                Console.WriteLine("------------------------------------------------------------");
+                fire[1].ChangeCouleur(); fire[1].ChangeCouleur(); fire[1].ChangeEtat(); Console.WriteLine(fire[1].AfficheTout());
+                Console.WriteLine("Feu Clignotant : ");
+                Console.WriteLine("---------------------");
+                fire[1].Clignote(5);
+                Console.WriteLine("Changement de Couleur : ");
+                Console.WriteLine("---------------------");
+                fire[0].ClignoteCouleur(4);
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
