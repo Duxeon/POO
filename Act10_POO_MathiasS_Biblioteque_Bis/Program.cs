5e6b5268503fc07d1dd1358c8c0c8716fc35848e
Duxeon <duxeon000@gmail.com>
2025-01-14 10:17:13 +0100

    vg

---
+++ Act10_POO_MathiasS_Biblioteque_Bis/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Act10_POO_MathiasS_Biblioteque_Bis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
