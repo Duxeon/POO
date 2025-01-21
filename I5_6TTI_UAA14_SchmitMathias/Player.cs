96d7fcce58372da96161877f3f3e3e804f4bbbe1
Duxeon <duxeon000@gmail.com>
2025-01-21 11:22:52 +0100

    I5

---
+++ I5_6TTI_UAA14_SchmitMathias/Player.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I5_6TTI_UAA14_SchmitMathias
+{
+    internal class Player
+    {
+        private string _pseudo;
+        private byte _nbCartoucheEnPoche;
+        private PaintBallGun _myPaintBallGun;
+
+        public string Pseudo
+        {
+            get => _pseudo;
+        }
+        public byte NbCartoucheEnPoche
+        {
+            get => _nbCartoucheEnPoche;
+            set
+            {
+                _nbCartoucheEnPoche = value;
+            }
+        }
+        public PaintBallGun MyPaintBallGun
+        {
+            get => _myPaintBallGun;
+            set
+            {
+                _myPaintBallGun = value;
+            }
+        }
+
+        public Player(string pseudo, PaintBallGun pbg)
+        {
+            _pseudo = pseudo;
+            _myPaintBallGun = pbg;
+            _nbCartoucheEnPoche = 30;
+        }
+
+        public string Recharge()
+        {
+            if (_myPaintBallGun.BallesChargeur == _myPaintBallGun.TailleChargeur)
+            {
+                return "Le chargeur est plein";
+            }
+            byte r = (byte)(MyPaintBallGun.TailleChargeur - MyPaintBallGun.BallesChargeur);
+            if (r > _nbCartoucheEnPoche)
+            {
+                r = _nbCartoucheEnPoche;
+            }
+            _myPaintBallGun.BallesChargeur += r;
+            _nbCartoucheEnPoche -= r;
+            return "Recharge de " + r + " balles dans le chargeur effectuée";
+        }
+        public bool Tire()
+        {
+            if (!MyPaintBallGun.IsVide())
+            {
+                MyPaintBallGun.BallesChargeur--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    internal class PaintBallGun
+    {
+        private byte _nbBallesChargeur;
+        private byte _tailleChargeur;
+
+        public byte BallesChargeur
+        {
+            get { return _nbBallesChargeur; }
+            set { _nbBallesChargeur = value; }
+        }
+        public byte TailleChargeur
+        {
+            get => _tailleChargeur;
+        }
+
+        public PaintBallGun()
+        {
+            _nbBallesChargeur = 0;
+            _tailleChargeur = 16;
+        }
+
+        public bool IsVide()
+        {
+            if (_nbBallesChargeur == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ I5_6TTI_UAA14_SchmitMathias/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Numerics;
+
+namespace I5_6TTI_UAA14_SchmitMathias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue dans ce jeu de tir ... Vous démarrez avec 30 balles");
+            Console.WriteLine("=============================================================");
+            PaintBallGun[] pbg = new PaintBallGun[30];
+            for (var i = 0; i < pbg.Length; i++)
+            {
+                pbg[i] = new PaintBallGun();
+            }
+            Random random = new Random();
+            Player player = new Player("Luigi", pbg[random.Next(0, pbg.Length)]);
+            bool q = true;
+            do
+            {
+                if (player.MyPaintBallGun.IsVide())
+                {
+                    Console.WriteLine("Attention votre chargeur est vide \n");
+                }
+                Console.WriteLine("Espace pour tirer,\nr pour recharger,\nv pour voir combien il reste de munitions en poche et dans le chargeur,\n+ pour reprendre des munitions,\nq pour quitter\n--->");
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.Spacebar:
+                        if (player.Tire())
+                        {
+                            Console.WriteLine("=> Tir effectué !");
+                        }
+                        else
+                        {
+                            Console.WriteLine("=> Echec du tir, charcheur vide");
+                        }
+                        break;
+                    case ConsoleKey.R:
+                        Console.WriteLine("=> " + player.Recharge());
+                        break;
+                    case ConsoleKey.V:
+                        Console.WriteLine("=> Vous avez un total de " + player.MyPaintBallGun.BallesChargeur + " cartouches dans le chargeur et " + player.NbCartoucheEnPoche + " balles dans le chargeur");
+                        break;
+                    case ConsoleKey.Add:
+                        player.NbCartoucheEnPoche += 30;
+                        Console.WriteLine("=> Reprise de 30 cartouches effectuée, vous avez un total de " + player.NbCartoucheEnPoche + " cartouches en poche.");
+                        break;
+                    case ConsoleKey.Q:
+                        q = false;
+                        break;
+                    default:
+                        break;
+                }
+            } while (q);
+        }
+    }
+}
